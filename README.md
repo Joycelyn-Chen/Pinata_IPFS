fae3974e37e477b59ef46d2ba08b2ed9fba9d495
megh1202 <megh12@student.ubc.ca>
2023-03-24 10:11:07 -0700

    chore: initial setup

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # Pinata_IPFS
 
+This code is built on [alchemy](https://alchemy.com) and [pinata](https://www.pinata.cloud).
+
+To deploy the smart contract on Goerli
+```bash
+npx hardhat run --network goerli scripts/deploy.js
+``` 
+
+To set up the repository and run the marketplace locally
+First create a account on [alchemy](https://alchemy.com) and [pinata](https://www.pinata.cloud) and create a `.env` file and store the 
+```
+REACT_APP_PINATA_KEY=
+REACT_APP_PINATA_SECRET=
+REACT_APP_ALCHEMY_API_URL=
+REACT_APP_PRIVATE_KEY=
+```
+run the below command
+```bash
+git clone https://github.com/SoftTalents/PetDog_NFT_Marketplace.git
+cd PetDog_NFT_Marketplace
+npm install
+npm start
+```
+
+
+
 ## Problem being solved: 
 - Traceability: When photos are uploaded, there is a CID attached to that photo such that no one else can claim your work as their own.
 - Storage limits: If your local storage is limited, you can upload your info to a different node and utilize that storage (cannot choose which node to store it on). 25gb upload per pin. 
--- src/App.js
+++ src/App.js
@@ -1,87 +1,25 @@
-import { useState } from 'react';
-import FormData from 'form-data';
-import axios from 'axios';
-
+import './App.css';
+import Navbar from './components/Navbar.js';
+import Marketplace from './components/Marketplace';
+import Profile from './components/Profile';
+import SellNFT from './components/SellNFT';
+import NFTPage from './components/NFTpage';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-
-  const [file, setFile] = useState()
-  const [myipfsHash, setIPFSHASH] = useState('')
-  
-  var config = {
-    method: 'get',
-    url: 'https://api.pinata.cloud/data/pinList?status=pinned&pinSizeMin=100',
-    headers: { 
-      'Authorization': process.env.JWT_AUTH_TOKEN
-    }
-  };
-  
-  
-  
-
-  const handleFile=async (fileToHandle) =>{
-
-    
-
-    console.log('starting')
-
-    // initialize the form data
-    const formData = new FormData()
-
-    // append the file form data to 
-    formData.append("file", fileToHandle)
-
-    // call the keys from .env
-
-    const API_KEY = process.env.REACT_APP_API_KEY
-    const API_SECRET = process.env.REACT_APP_API_SECRET
-
-    // the endpoint needed to upload the file
-    const url =  `https://api.pinata.cloud/pinning/pinFileToIPFS`
-
-    const response = await axios.post(
-      url,
-      formData,
-      {
-          maxContentLength: "Infinity",
-          headers: {
-              "Content-Type": `multipart/form-data;boundary=${formData._boundary}`, 
-              'pinata_api_key': API_KEY,
-              'pinata_secret_api_key': API_SECRET
-
-          }
-      }
-  )
-
-  console.log(response)
-
-  // get the hash
-  setIPFSHASH(response.data.IpfsHash)
-  try{
-  const result = await axios(config);
-  console.log("resp"+result);
-  } catch(e){
-    console.log(e)
-  }
-  
-  
-  }
-
-  
-
   return (
-    <div className="App">
-      <input type="file" onChange={(event)=>setFile(event.target.files[0])}/>
-      <button onClick={()=>handleFile(file)}>Pin</button>
-      
-      
-    {
-
-      //  render the hash
-      myipfsHash.length > 0 && <img height='200' src={`https://gateway.pinata.cloud/ipfs/${myipfsHash}`} alt='not loading'/>
-    }
-    
-
+    <div className="container">
+        <Routes>
+          <Route path="/" element={<Marketplace />}/>
+          <Route path="/nftPage" element={<NFTPage />}/>        
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sellNFT" element={<SellNFT />}/>             
+        </Routes>
     </div>
   );
 }
