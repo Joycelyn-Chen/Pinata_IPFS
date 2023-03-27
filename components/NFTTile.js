8cd97a4d151ee7e8b4ac13d1a2f7fc15a260a757
m-basha98 <91850741+m-basha98@users.noreply.github.com>
2023-03-27 13:39:56 -0700

    Add files via upload

---
+++ components/NFTTile.js
@@ -0,0 +1,26 @@
+import axie from "../tile.jpeg";
+import {
+    BrowserRouter as Router,
+    Link,
+  } from "react-router-dom";
+
+function NFTTile (data) {
+    const newTo = {
+        pathname:"/nftPage/"+data.data.tokenId
+    }
+    return (
+        <Link to={newTo} className="border-2 ml-10 mr-10 mt-5 mb-12 flex flex-col items-center hover:opacity-50 rounded-lg">
+        <div className=" w-48 md:w-72 shadow-lg shadow-pink-200 ">
+            <img src={data.data.image} alt="" className="w-72 h-80 rounded-lg object-cover" />
+            <div className= "text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
+                <strong className="text-xl">{data.data.name}</strong>
+                <p className="display-inline">
+                    {data.data.description}
+                </p>
+            </div>
+        </div>
+        </Link>
+    )
+}
+
+export default NFTTile;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,26 @@
-const { ethers } = require("hardhat");
-const hre = require("hardhat");
-const fs = require("fs");
+// Importing packages
+const { ethers } = require("hardhat"); // ethers library to interact with Ethereum contracts
+const hre = require("hardhat"); // Hardhat Runtime Environment package
+const fs = require("fs"); // Node.js file system package
 
+// Define an async function to deploy the NFT marketplace contract
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  const balance = await deployer.getBalance();
+  const [deployer] = await ethers.getSigners(); // Get the signer account (deployer) from Hardhat network
+  const balance = await deployer.getBalance(); // Get the account balance of the deployer
+
+  // Get the NFTMarketplace contract factory instance using Hardhat ethers
   const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-  const marketplace = await Marketplace.deploy();
+  const marketplace = await Marketplace.deploy(); // Deploy the NFTMarketplace contract
 
-  await marketplace.deployed();
+  await marketplace.deployed(); // Wait for the contract deployment to complete
 
+  // Store the contract address and ABI in a JSON object
   const data = {
     address: marketplace.address,
     abi: JSON.parse(marketplace.interface.format('json'))
   }
 
-  //This writes the ABI and address to the mktplace.json
+  // Write the contract address and ABI to a JSON file (Marketplace.json)
   fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
 }
 
